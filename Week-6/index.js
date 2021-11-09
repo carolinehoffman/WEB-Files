1c70530c8aa177d2c8d723893e39566866484fb3
carolinehoffman <caho5283@colorado.edu>
2021-11-08 20:34:24 -0700

    Update

---
+++ Week-6/index.js
@@ -0,0 +1,10 @@
+document.getElementById("mybutton").addEventListener("click", buttonOne);
+document.getElementById("mybutton2").addEventListener("click", buttonTwo);
+
+function buttonOne(){
+  alert("@carolineghoffman");
+  /*a href=https://www.instagram.com/carolineghoffman/;*/
+}
+function buttonTwo(){
+  alert("https://carolinehoffman412.wixsite.com/portfolio");
+}
