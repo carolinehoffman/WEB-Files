e35ac18467b74c4f22642ac3d5ce9f2cf4834ae1
carolinehoffman <caho5283@colorado.edu>
2021-12-02 10:53:37 -0700

    Update

--- Final-Project/index.js
+++ Final-Project/index.js
@@ -44,3 +44,8 @@ function contact(){
         document.getElementById(newItems[i]).style.display="none";
     }
 }
+
+document.getElementById("submitButton").addEventListener("click", submit);
+function submit(){
+  alert("Thanks for reaching out! I'll get back to you as soon as I can.")
+}
